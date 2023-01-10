00a54e979bfb23a02ee88927272b081418a1b44f
Adcode1 <113816770+Adcode1@users.noreply.github.com>
2023-01-10 23:51:51 +0530

    Create partition.py

---
+++ partition.py
@@ -0,0 +1,18 @@
+def swap(i,j,lst):
+    temp=lst[i]
+    lst[i]=lst[j]
+    lst[j]=temp
+def partition(lst,x):
+    i=0
+    j=(len(lst)-1)
+    while i<j and lst[i]<=x:
+        i=i+1
+    while i<j and lst[j]>=x:
+        j=j-1
+    while i<j:
+        swap(i,j,lst)
+        while i<j and lst[i]<=x:
+            i=i+1
+        while i<j and lst[j]>=x:
+            j=j-1
+    (p1,p2)=(lst[0:j],lst[j:len(lst)])
