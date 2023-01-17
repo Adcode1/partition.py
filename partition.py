c20892dea383d4018d4971e9be624ea01876365c
Adcode1 <113816770+Adcode1@users.noreply.github.com>
2023-01-18 01:33:40 +0530

    Update partition.py

--- partition.py
+++ partition.py
@@ -5,14 +5,14 @@ def swap(i,j,lst):
 def partition(lst,x):
     i=0
     j=(len(lst)-1)
-    while i<j and lst[i]<=x:
+    while i<j and lst[i]<x:
         i=i+1
     while i<j and lst[j]>=x:
         j=j-1
     while i<j:
         swap(i,j,lst)
-        while i<j and lst[i]<=x:
+        while i<j and lst[i]<x:
             i=i+1
         while i<j and lst[j]>=x:
-            j=j-1
+            continue
     (p1,p2)=(lst[0:j],lst[j:len(lst)])
